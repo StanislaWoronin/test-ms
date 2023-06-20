ecb311021f98d892c4dec211b2b67777863aead3
Stanislav <stanislaworonin@yandex.ru>
2023-06-20 18:43:34 +0300

    complite

--- apps/api-getaway/src/main.ts
+++ apps/api-getaway/src/main.ts
@@ -1,8 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions } from '@nestjs/microservices';
 import { ApiGetawayModule } from './api-getaway.module';
-import { Microservices } from '../../../libs/shared';
-import { getTransportOptions } from '../../../libs/options';
 
 const port = 3000;
 
